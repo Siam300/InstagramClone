122785fbdf9582fad840f8b1db3658ff454847c8
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-12-10 20:26:39 +0600

    Created instagram project and basic tabbar view added.

---
+++ Instagram/Core/TabBar/MainTabView.swift
@@ -0,0 +1,46 @@
+//
+//  MainTabView.swift
+//  Instagram
+//
+//  Created by Auto on 12/10/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Upload Posts")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(Color.black)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
