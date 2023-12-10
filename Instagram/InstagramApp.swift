fd6e0b9eb9812bc8c29d88618fdba13c3c84c4d0
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-12-10 19:58:49 +0600

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Auto on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
