e7bb13e255d708174050b8b38e7ffe0cda2fbc61
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-12-12 21:54:32 +0600

    firebase and auth service implemented.

---
+++ Instagram/Core/Authintication/Service/AuthService.swift
@@ -0,0 +1,36 @@
+//
+//  AuthService.swift
+//  Instagram
+//
+//  Created by Auto on 12/12/23.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthService {
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    static let shared = AuthService()
+    
+    init() {
+        self.userSession = Auth.auth().currentUser
+    }
+    
+    func login(withemail email: String, password: String) async throws {
+        
+    }
+    
+    func createUser(email: String, password: String, username: String) async throws {
+        
+    }
+    
+    func loadUserData() async throws {
+        
+    }
+    
+    func signOut() {
+        
+    }
+}
