a906eb0d3dc591ee0d89e8e824579ec967dfebb2
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-12-16 02:54:55 +0600

    Comments ui view added.

---
+++ Instagram/Core/Comments/View/CommentCellView.swift
@@ -0,0 +1,20 @@
+//
+//  CommentCellVIew.swift
+//  Instagram
+//
+//  Created by Auto on 12/16/23.
+//
+
+import SwiftUI
+
+struct CommentCellVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommentCellVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        CommentCellVIew()
+    }
+}
---
+++ Instagram/Core/Comments/View/CommentsView.swift
@@ -0,0 +1,20 @@
+//
+//  CommentsView.swift
+//  Instagram
+//
+//  Created by Auto on 12/16/23.
+//
+
+import SwiftUI
+
+struct CommentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommentsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommentsView()
+    }
+}
